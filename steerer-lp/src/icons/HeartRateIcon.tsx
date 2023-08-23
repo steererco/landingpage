308b5d7f5678640be87780e10257fe80e6696836
mouraa0 <jpcmouraa@gmail.com>
2023-08-23 11:38:01 -0400

    feat: create heart rate icon

---
+++ steerer-lp/src/icons/HeartRateIcon.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const HeartRateIcon = ({ ...props }: React.SVGProps<SVGSVGElement>): JSX.Element => (
+    <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+        <path d="M22 12H18L15 21L9 3L6 12H2" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+)
