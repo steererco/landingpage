da90ec87dcbfd5ec8246dba4b877a8352075ef7e
mouraa0 <jpcmouraa@gmail.com>
2023-08-24 15:23:27 -0400

    feat: set DefaultButton font

--- steerer-lp/src/components/DefaultButton.tsx
+++ steerer-lp/src/components/DefaultButton.tsx
@@ -2,7 +2,7 @@ import { MouseEventHandler } from "react";
 
 export const DefaultButton = ({ label, onClick }: { label: string, onClick: MouseEventHandler<HTMLButtonElement>; }): JSX.Element => {
     return (
-        <button onClick={onClick} className="bg-base-blue3 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-[5px]">
+        <button onClick={onClick} className="bg-base-blue3 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-[5px] font-inter font-semibold">
             {label}
         </button>
     )
