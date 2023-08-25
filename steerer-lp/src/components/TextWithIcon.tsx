19c614d179600474452ddbe37c54c6e333fa195c
mouraa0 <jpcmouraa@gmail.com>
2023-08-24 22:35:17 -0400

    fix: make TextWithIcon a row

--- steerer-lp/src/components/TextWithIcon.tsx
+++ steerer-lp/src/components/TextWithIcon.tsx
@@ -1,10 +1,10 @@
 import { ReactNode } from "react";
 
 export const TextWithIcon = ({ icon, children }: { icon: JSX.Element, children?: ReactNode }): JSX.Element => (
-    <>
+    <div className="flex flex-row justify-center items-center">
         <div className="bg-base-blue2/[0.15] rounded-5px w-[60px] h-[60px] flex items-center justify-center mr-[24px]">
             {icon}
         </div>
         <p className="text-black4 font-inter font-medium" >{children}</p>
-    </>
+    </div>
 )
