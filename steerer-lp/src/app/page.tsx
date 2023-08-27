e34ac72b59c9c78c606e3a45c27e54290ccf2ebe
mouraa0 <jpcmouraa@gmail.com>
2023-08-27 06:22:11 -0300

    feat: create containers in TextImageColumn

--- steerer-lp/src/app/page.tsx
+++ steerer-lp/src/app/page.tsx
@@ -1,12 +1,14 @@
 import { TextRow } from "./components/TextRow"
 import { Header } from "@/components/Header"
 import { TextColumnsRow } from "./components/TextColumnsRow"
+import { TextImageColumn } from "./components/TextImageColumn"
 
 const Home = (): JSX.Element => {
   return (
     <main className="bg-white h-screen">
       <Header />
       <TextRow />
+      <TextImageColumn />
       <TextColumnsRow />
     </main>
   )
