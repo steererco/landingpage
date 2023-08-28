b2958aca45d22c906c1ecc3c184ac657a0747f22
mouraa0 <jpcmouraa@gmail.com>
2023-08-27 23:49:47 -0300

    feat: add medium weight to poppins font

--- steerer-lp/src/app/layout.tsx
+++ steerer-lp/src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins, Inter } from 'next/font/google'
 
-const poppins = Poppins({ weight: ["600", "700"], subsets: ["latin-ext"], variable: "--font-poppins" })
+const poppins = Poppins({ weight: ["500", "600", "700"], subsets: ["latin-ext"], variable: "--font-poppins" })
 const inter = Inter({ weight: ["400", "500", "600"], subsets: ["latin-ext"], variable: "--font-inter" })
 
 export const metadata: Metadata = {
