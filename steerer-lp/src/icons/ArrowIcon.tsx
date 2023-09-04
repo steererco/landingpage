329327ffa8899f7695a78e2128c4302f86d6b4dc
mouraa0 <jpcmouraa@gmail.com>
2023-09-04 13:17:55 -0300

    feat: add ArrowIcon

---
+++ steerer-lp/src/icons/ArrowIcon.tsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+
+export const ArrowIcon = ({ ...props }: React.SVGProps<SVGSVGElement>): JSX.Element => (
+    <svg width="22" height="14" viewBox="0 0 22 14" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+        <path d="M1 6.97192L20.6217 6.97193" stroke="#F7F7F7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M15.0281 1L20.9999 6.97182L15.0281 12.9436" stroke="#F7F7F7" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+)
