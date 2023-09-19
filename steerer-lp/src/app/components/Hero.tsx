589e8a2d617bf6be682e201d5ca1750661d63fda
mouraa0 <jpcmouraa@gmail.com>
2023-09-19 05:39:13 -0300

    feat: add page title in desktop

--- steerer-lp/src/app/components/Hero.tsx
+++ steerer-lp/src/app/components/Hero.tsx
@@ -1,10 +1,12 @@
 import { Header } from "@/components/Header";
 import { TextRow } from "./TextRow";
 import { Dispatch, SetStateAction } from "react";
+import { TextWithUnderline } from "@/components/TextWithUnderline";
 
 export const Hero = ({ openDrawer, setOpenDrawer }: { openDrawer: boolean, setOpenDrawer: Dispatch<SetStateAction<boolean>> }): JSX.Element => (
-    <div>
+    <div className="">
         <Header isOpen={openDrawer} setIsOpen={setOpenDrawer} />
+        <TextWithUnderline />
         <TextRow />
     </div>
 )
