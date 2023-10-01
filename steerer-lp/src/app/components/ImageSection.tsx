d3acb935a30404ed44dbc9e0242e733b33aaf93f
mouraa0 <jpcmouraa@gmail.com>
2023-10-01 04:31:55 -0300

    feat: advances in ImageSection

--- steerer-lp/src/app/components/ImageSection.tsx
+++ steerer-lp/src/app/components/ImageSection.tsx
@@ -8,13 +8,15 @@ import { WhiteLogo } from "@/logo/WhiteLogo"
 import { WaitingListButton } from "@/components/WaitingListButton"
 
 export const ImageSection = (): JSX.Element => (
-    <div className="flex flex-row gap-[30px] h-screen items-center mobile:px-20 px-5">
-        <div className="flex flex-1 h-[70%] flex-col gap-[30px] h-full">
-            <FirstRow />
-            <SecondRow />
+    <div className="flex flex-row gap-[20px] mobile:gap-[30px] overflow-y-scroll no-scrollbar mobile:overflow-auto h-screen items-center mobile:px-20 px-5">
+        <div className="flex flex-1 h-[70%] gap-[30px]">
+            <div className="flex flex-col h-full w-screen mobile:w-full gap-[30px]">
+                <FirstRow />
+                <SecondRow />
+            </div>
         </div>
-        <div className="flex flex-1 h-[70%] flex-row gap-[30px]">
-            <div className="w-full h-full rounded-[20px] bg-gradient-to-b from-base-blue3 to-base-blue1">
+        <div className="flex flex-1 h-[70%] flex-row gap-[20px] mobile:gap-[30px]">
+            <div className="w-[38vh] mobile:w-full h-full rounded-[20px] bg-gradient-to-b from-base-blue3 to-base-blue1">
                 <div className="flex flex-col h-full px-[18px] justify-around items-center">
                     <WhiteLogo />
                     <p className="font-poppins font-medium text-[34px] text-center">Awaken your greatness in soccer with the Steerer app!</p>
@@ -44,7 +46,7 @@ const SecondRow = (): JSX.Element => (
 )
 
 const SecondCol = (): JSX.Element => (
-    <div className="flex flex-col w-full gap-[30px]">
+    <div className="flex flex-col w-screen mobile:w-full gap-[30px]">
         <div className="flex rounded-[20px]">
             <Image src={twoPlayersMockup} alt="" />
         </div>
