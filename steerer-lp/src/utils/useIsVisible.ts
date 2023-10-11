f6bc93f73f75b541a97c301abd52a752ebc14185
mouraa0 <jpcmouraa@gmail.com>
2023-10-11 15:35:33 -0300

    feat: create useIsVisible hook

---
+++ steerer-lp/src/utils/useIsVisible.ts
@@ -0,0 +1,19 @@
+import { MutableRefObject, useEffect, useState } from "react";
+
+export function useIsVisible(ref: any) {
+    const [isIntersecting, setIntersecting] = useState(false);
+  
+    useEffect(() => {
+      const observer = new IntersectionObserver(([entry]) => {
+          setIntersecting(entry.isIntersecting)
+      } 
+      );
+      
+      observer.observe(ref.current);
+      return () => {
+        observer.disconnect();
+      };
+    }, [ref]);
+  
+    return isIntersecting;
+  }
