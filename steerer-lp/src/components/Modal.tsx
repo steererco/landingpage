18752b9482d043144b906ad02aebd40a4f604655
mouraa0 <jpcmouraa@gmail.com>
2023-10-12 16:28:03 -0300

    feat: Modal

---
+++ steerer-lp/src/components/Modal.tsx
@@ -0,0 +1,50 @@
+import { ModalContext } from "@/context/ModalContext";
+import { CloseIcon } from "@/icons/CloseIcon";
+import { useContext } from "react";
+import { Clickable } from "./Clickable";
+import { DefaultButton } from "./DefaultButton";
+
+export const Modal = (): JSX.Element | null => {
+    const context = useContext(ModalContext)
+
+    if (!context.isOpen) return null;
+
+    return (
+        <>
+            {context.isOpen && <></>}
+            <div className="fixed scale-100 inset-0 flex justify-center items-center bg-black/[.5] z-[1]">
+                <div className="flex flex-col w-[360px] h-[466px] rounded-[20px] bg-white px-6 pt-3 pb-6">
+                    <Close onClick={() => context.changeIsOpen(false)} />
+                    <Input placeholder="Name:" id="name" type="text" />
+                    <Input placeholder="E-mail:" id="email" type="email" />
+                    <Input placeholder="WhatsApp:" id="wpp" type="tel" />
+                    <div className="h-6"></div>
+                    <Button />
+                    <p className="text-black/[.7] text-left font-inter font-medium text-[10px] leading-4 mt-6">
+                        By signing up, you agree to the processing of your personal data to receive communications and emails from the company. Your data will only be used for communication and marketing purposes. You can unsubscribe from these emails at any time. For more information, please check our Privacy Notice.
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+const Close = ({ onClick }: { onClick: () => void }): JSX.Element => (
+    <div className="self-end">
+        <Clickable onClick={onClick}>
+            < CloseIcon />
+        </Clickable>
+    </div>
+)
+
+const Input = ({ placeholder, type, id }: { placeholder: string, type: string, id: string }): JSX.Element => (
+    <input className="mt-4 w-full h-[60px] rounded-[10px] border-2 border-gray1 placeholder-gray2 font-inter font-medium text-black2 pl-4 pr-4" id={id} type={type} placeholder={placeholder} />
+)
+
+const Button = (): JSX.Element => (
+    <DefaultButton onClick={() => console.log('hehe')}>
+        <p className="h-[36px] flex items-center justify-center">
+            CONFIRM SUBSCRIPTION
+        </p>
+    </DefaultButton>
+)
