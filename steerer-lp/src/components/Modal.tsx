8d1486ecd87281efe3ed3ceb310cc1772bd4ebd9
mouraa0 <jpcmouraa@gmail.com>
2023-10-12 16:49:40 -0300

    feat: Modal animations

--- steerer-lp/src/components/Modal.tsx
+++ steerer-lp/src/components/Modal.tsx
@@ -7,25 +7,20 @@ import { DefaultButton } from "./DefaultButton";
 export const Modal = (): JSX.Element | null => {
     const context = useContext(ModalContext)
 
-    if (!context.isOpen) return null;
-
     return (
-        <>
-            {context.isOpen && <></>}
-            <div className="fixed scale-100 inset-0 flex justify-center items-center bg-black/[.5] z-[1]">
-                <div className="flex flex-col w-[360px] h-[466px] rounded-[20px] bg-white px-6 pt-3 pb-6">
-                    <Close onClick={() => context.changeIsOpen(false)} />
-                    <Input placeholder="Name:" id="name" type="text" />
-                    <Input placeholder="E-mail:" id="email" type="email" />
-                    <Input placeholder="WhatsApp:" id="wpp" type="tel" />
-                    <div className="h-6"></div>
-                    <Button />
-                    <p className="text-black/[.7] text-left font-inter font-medium text-[10px] leading-4 mt-6">
-                        By signing up, you agree to the processing of your personal data to receive communications and emails from the company. Your data will only be used for communication and marketing purposes. You can unsubscribe from these emails at any time. For more information, please check our Privacy Notice.
-                    </p>
-                </div>
+        <div className={`fixed inset-0 flex justify-center items-center z-[1] transition-colors ${context.isOpen ? "visible bg-black/[.5]" : "invisible"}`}>
+            <div className={`flex flex-col w-[360px] h-[466px] rounded-[20px] bg-white px-6 pt-3 pb-6 transition-all ${context.isOpen ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}>
+                <Close onClick={() => context.changeIsOpen(false)} />
+                <Input placeholder="Name:" id="name" type="text" />
+                <Input placeholder="E-mail:" id="email" type="email" />
+                <Input placeholder="WhatsApp:" id="wpp" type="tel" />
+                <div className="h-6"></div>
+                <Button />
+                <p className="text-black/[.7] text-left font-inter font-medium text-[10px] leading-4 mt-6">
+                    By signing up, you agree to the processing of your personal data to receive communications and emails from the company. Your data will only be used for communication and marketing purposes. You can unsubscribe from these emails at any time. For more information, please check our Privacy Notice.
+                </p>
             </div>
-        </>
+        </div>
     )
 }
 
