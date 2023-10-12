983613eeaf47cfbb9f3a358c94d4334fa93afb95
mouraa0 <jpcmouraa@gmail.com>
2023-10-12 16:27:54 -0300

    feat: open modal on WaitingListButton click using ModalContext

--- steerer-lp/src/components/WaitingListButton.tsx
+++ steerer-lp/src/components/WaitingListButton.tsx
@@ -1,11 +1,20 @@
 import { ArrowIcon } from "@/icons/ArrowIcon";
 import { DefaultButton } from "./DefaultButton";
+import { useContext } from "react";
+import { ModalContext } from "@/context/ModalContext";
 
-export const WaitingListButton = (): JSX.Element => (
-    <DefaultButton onClick={() => { }}>
-        <div className="flex flex-row items-center">
-            <p className="pr-6">Join the waiting list</p>
-            <ArrowIcon />
-        </div>
-    </DefaultButton>
-)
+export const WaitingListButton = (): JSX.Element => {
+    const context = useContext(ModalContext)
+
+    return (
+        <>
+            <DefaultButton onClick={() => context.changeIsOpen(true)}>
+                <div className="flex flex-row items-center">
+                    <p className="pr-6">Join the waiting list</p>
+                    <ArrowIcon />
+                </div>
+            </DefaultButton>
+        </>
+    )
+}
+
