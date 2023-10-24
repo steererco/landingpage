859841241fea307c37eff661d636c6e56c3eef2f
mouraa0 <jpcmouraa@gmail.com>
2023-10-24 20:32:16 -0300

    feat: initial use of react hook form

--- steerer-lp/src/components/Modal.tsx
+++ steerer-lp/src/components/Modal.tsx
@@ -2,25 +2,42 @@ import { ModalContext } from "@/context/ModalContext";
 import { CloseIcon } from "@/icons/CloseIcon";
 import { useContext } from "react";
 import { Clickable } from "./Clickable";
-import { DefaultButton } from "./DefaultButton";
+import { useForm, SubmitHandler, UseFormRegister } from "react-hook-form"
+import { useEffect } from "react";
+
+interface IFormInput {
+    name: string
+    email: string
+    phoneNumber: string
+}
 
 export const Modal = (): JSX.Element | null => {
+    const { register, handleSubmit, watch } = useForm<IFormInput>({ mode: 'onChange' })
     const context = useContext(ModalContext)
 
+    const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data)
+
+    useEffect(() => {
+        console.log(watch('name'))
+    }, [watch('name')])
+
+
     return (
-        <div className={`fixed inset-0 flex justify-center items-center z-[1] transition-colors ${context.isOpen ? "visible bg-black/[.5]" : "invisible"}`}>
-            <div className={`flex flex-col w-[360px] h-[466px] rounded-[20px] bg-white px-6 pt-3 pb-6 transition-all ${context.isOpen ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}>
-                <Close onClick={() => context.changeIsOpen(false)} />
-                <Input placeholder="Name:" id="name" type="text" />
-                <Input placeholder="Email:" id="email" type="email" />
-                <Input placeholder="Phone number:" id="wpp" type="tel" />
-                <div className="h-6"></div>
-                <Button />
-                <p className="text-black/[.7] text-left font-inter font-medium text-[10px] leading-4 mt-6">
-                    By signing up, you agree to the processing of your personal data to receive communications and emails from the company. Your data will only be used for communication and marketing purposes. You can unsubscribe from these emails at any time. For more information, please check our Privacy Notice.
-                </p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={`fixed inset-0 flex justify-center items-center z-[1] transition-colors ${context.isOpen ? "visible bg-black/[.5]" : "invisible"}`}>
+                <div className={`flex flex-col w-[360px] h-[466px] rounded-[20px] bg-white px-6 pt-3 pb-6 transition-all ${context.isOpen ? "scale-100 opacity-100" : "scale-125 opacity-0"}`}>
+                    <Close onClick={() => context.changeIsOpen(false)} />
+                    <Input register={register} name='name' type="text" placeholder="Name:" />
+                    <Input register={register} name='email' placeholder="Email:" type="email" />
+                    <Input register={register} name='phoneNumber' placeholder="Phone number:" type="tel" />
+                    <div className="h-6" />
+                    <Button type="submit" />
+                    <p className="text-black/[.7] text-left font-inter font-medium text-[10px] leading-4 mt-6">
+                        By signing up, you agree to the processing of your personal data to receive communications and emails from the company. Your data will only be used for communication and marketing purposes. You can unsubscribe from these emails at any time. For more information, please check our Privacy Notice.
+                    </p>
+                </div>
             </div>
-        </div>
+        </form>
     )
 }
 
@@ -32,14 +49,12 @@ const Close = ({ onClick }: { onClick: () => void }): JSX.Element => (
     </div>
 )
 
-const Input = ({ placeholder, type, id }: { placeholder: string, type: string, id: string }): JSX.Element => (
-    <input className="mt-4 w-full h-[60px] rounded-[10px] border-2 border-gray1 placeholder-gray2 font-inter font-medium text-black2 pl-4 pr-4" id={id} type={type} placeholder={placeholder} />
+type InputProps = React.InputHTMLAttributes<HTMLInputElement> & { register: UseFormRegister<IFormInput>, name: keyof IFormInput }
+
+const Input = ({ register, name, ...props }: InputProps) => (
+    <input {...register(name)} className="mt-4 w-full h-[60px] rounded-[10px] border-2 border-gray1 placeholder-gray2 font-inter font-medium text-black2 pl-4 pr-4" {...props} />
 )
 
-const Button = (): JSX.Element => (
-    <DefaultButton onClick={() => console.log('hehe')}>
-        <p className="h-[36px] flex items-center justify-center">
-            CONFIRM SUBSCRIPTION
-        </p>
-    </DefaultButton>
+const Button = ({ ...props }: React.InputHTMLAttributes<HTMLInputElement>): JSX.Element => (
+    <input type="submit" value="CONFIRM SUBSCRIPTION" className="bg-base-blue3 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-[5px] font-inter font-semibold" {...props} />
 )
