3ef06c0dcf379e66a1576cee4c7743427c420f8e
Nikita Stogniy <nikitastognii@gmail.com>
2024-03-20 22:07:57 +0400

    PRIVACYANDTERMS

---
+++ steerer-lp/src/app/privacy/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { content } from "./privacy";
+const Page = () => {
+  return (
+    <div className="container mx-auto py-4 bg-white text-black">
+      <h1 className="text-3xl font-bold">Terms</h1>
+      <div dangerouslySetInnerHTML={{ __html: content }}></div>
+    </div>
+  );
+};
+
+export default Page;
