ab648f6954d60f99ec5a51c8810fa272a44b5f2e
nikitastogniy812 <nikitastognii@gmail.com>
2024-04-08 18:10:00 +0400

    Help page, logo in privacy and terms

--- steerer-lp/src/app/terms/page.tsx
+++ steerer-lp/src/app/terms/page.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { content } from "./terms";
+import { Header, Logo } from "@/components/Header";
+import Link from "next/link";
 const Page = () => {
   return (
     <div className="container mx-auto py-4 bg-white text-black">
+      <Link className="mb-4" href="/">
+        <Logo />
+      </Link>
       <h1 className="text-3xl font-bold">Terms</h1>
       <div dangerouslySetInnerHTML={{ __html: content }}></div>
     </div>
